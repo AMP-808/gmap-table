7be4890c865bdb28eeb77bbe816e6c87a7a37780
Dauvith Nespethal <DauvithNespethal@gmail.com>
2020-09-07 15:57:38 +0200

    fix map width on resize issue

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,120 +1,122 @@
-import React from 'react';
+import React from "react";
 import {
-    GoogleMap,
-    useLoadScript,
-    Marker,
-    InfoWindow
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
 } from "@react-google-maps/api";
 //import {formatRelative} from 'date-fns'
 // import "@reach/combobox/styles.css"
-require('dotenv').config();
+require("dotenv").config();
 
 const libraries = ["places"];
 const center = {
-    lat: 21.306944,
-    lng: -157.858337,
+  lat: 21.306944,
+  lng: -157.858337,
 };
 const options = {
-    /* disableDefaulUI: true,
+  /* disableDefaulUI: true,
     zoomControl: true */
-}
-
-
+};
 
 export default function Map(props) {
-    const [screenWidth, setScreenwidth] = React.useState({
-        width: `${window.innerWidth - 50}px`, //'95vw',
-        height: '500px',
-        //position: 'static',
-        margin: 'auto'
-        //textAlign: center,
-    })
-    const [selected, setSelected] = React.useState(null);
-    const mapRef = React.useRef();
-    const onMapLoad = React.useCallback((map) => {
-        mapRef.current = map;
-    })
+  const [selected, setSelected] = React.useState(null);
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  });
 
-    const mapContainerStyle = {
-        width: `100%`, //'95vw',
-        justifyContent: 'center',
-        height: '500px',
-        //position: 'static',
-        margin: 'auto'
-        //textAlign: center,
+  /* const mapContainerStyle = {
+    width: `100%`, //'95vw',
+    justifyContent: "center",
+    height: "500px",
+    //position: 'static',
+    margin: "auto",
+    //textAlign: center,
+  }; */
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyC8vjNCOlmexV02HRdjf_bGBRPWVmmx8s8",
+    libraries,
+  });
+
+  const [dimensions, setDimensions] = React.useState({
+    maxWidth: "950px",
+    width: `${window.innerWidth - 50}px`, //'95vw',
+    height: "500px",
+  });
+
+  function debounce(fn, ms) {
+    let timer;
+    return (_) => {
+      clearTimeout(timer);
+      timer = setTimeout((_) => {
+        timer = null;
+        fn.apply(this, arguments);
+      }, ms);
     };
+  }
 
-    const {isLoaded, loadError} = useLoadScript({
-        googleMapsApiKey: "AIzaSyBvB4ahNz4FIFFYiOeqPVgeAJfqloxsODw",
-        libraries,
-    });
-
-    const [dimensions, setDimensions] = React.useState({
+  //rerender the map on window resize from: https://www.pluralsight.com/guides/re-render-react-component-on-window-resize
+  React.useEffect(() => {
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDimensions({
+        maxWidth: "950px",
         width: `${window.innerWidth - 50}px`, //'95vw',
-        height: '500px',
-    })
+        height: "500px",
+      });
+    }, 1000);
 
+    window.addEventListener("resize", debouncedHandleResize);
 
-    function debounce(fn, ms) {
-        let timer
-        return _ => {
-          clearTimeout(timer)
-          timer = setTimeout(_ => {
-            timer = null
-            fn.apply(this, arguments)
-          }, ms)
-        };
-      }
+    return (_) => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    };
+  });
 
+  if (loadError) return "Error loading Maps";
+  if (!isLoaded) return "Loading Maps";
 
-      //rerender the map on window resize from: https://www.pluralsight.com/guides/re-render-react-component-on-window-resize
-      React.useEffect(() => {
-        const debouncedHandleResize = debounce(function handleResize() {
-          setDimensions({
-            width: `${window.innerWidth - 50}px`, //'95vw',
-            height: '500px',
-        })
-        }, 1000)
-    
-        window.addEventListener('resize', debouncedHandleResize)
-    
-        return _ => {
-          window.removeEventListener('resize', debouncedHandleResize)
-        
-    }});
-
-    if (loadError) return "Error loading Maps";
-    if (!isLoaded) return "Loading Maps"
-
-
-
-    return <div>
-        {/* <h1>FAPA.aero</h1> */}
-        <GoogleMap 
-            mapContainerStyle={dimensions}
-            zoom={8}
-            center={center}
-            options={options}
-            >
-                {props.data.map((marker) => (
-                    <Marker 
-                        position={{lat: marker.coordinates.lat, lng: marker.coordinates.lng}}
-                        onClick={() => {
-                            setSelected(marker)
-                        }}
-                    ></Marker>
-                ))}
-                {selected ? (<InfoWindow 
-                    position={{ lat: selected.coordinates.lat, lng: selected.coordinates.lng }} 
-                    onCloseClick={() => {
-                        setSelected(null);
-                    }}>
-                        <div style={{textAlign: 'left'}}>
-                            <h3>{selected.company_name}</h3>
-                            <p>{selected.address}</p>
-                            <a href={selected.link_to_apply} target="_blank">Apply</a>
-                        </div>
-                </InfoWindow>) : null}
-            </GoogleMap>
+  return (
+    <div>
+      {/* <h1>FAPA.aero</h1> */}
+      <GoogleMap
+        mapContainerStyle={dimensions}
+        zoom={8}
+        center={center}
+        options={options}
+      >
+        {props.data.map((marker) => (
+          <Marker
+            position={{
+              lat: marker.coordinates.lat,
+              lng: marker.coordinates.lng,
+            }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          ></Marker>
+        ))}
+        {selected ? (
+          <InfoWindow
+            position={{
+              lat: selected.coordinates.lat,
+              lng: selected.coordinates.lng,
+            }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div style={{ textAlign: "left" }}>
+              <h3>{selected.company_name}</h3>
+              <p>{selected.address}</p>
+              <a href={selected.link_to_apply} target="_blank">
+                Apply
+              </a>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
     </div>
+  );
 }
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 const airOpsTestList = [
   {
@@ -16,14 +16,14 @@ const airOpsTestList = [
     zip: "96819",
     number_of_planes: "3",
     yib: "2008-05-15",
-    ops_type: "PUB",
+    far_part: "135",
     link_to_apply: "https://www.alohaaircargo.com/careers/",
     coordinates: {
-      lat: 21.338280,
-      lng: -157.926730
-    }
-},
-{
+      lat: 21.33828,
+      lng: -157.92673,
+    },
+  },
+  {
     dsgn_code: "I5PA",
     company_name: "Asia Pacific Airlines",
     address: "155 Kapalulu Place, Suite 203",
@@ -32,14 +32,14 @@ const airOpsTestList = [
     zip: "96819",
     number_of_planes: "3",
     yib: "1999-06-03",
-    ops_type: "PUB",
+    far_part: "135",
     link_to_apply: "https://www.asiapacificairlines.com/careers/",
     coordinates: {
-      lat: 21.320590,
-      lng: -157.910800
-    }
-},
-{
+      lat: 21.32059,
+      lng: -157.9108,
+    },
+  },
+  {
     dsgn_code: "JRAA",
     company_name: "Trans Executive Airlines",
     address: "100 Iolana Place",
@@ -48,14 +48,14 @@ const airOpsTestList = [
     zip: "96819",
     number_of_planes: "10",
     yib: "1982-06-15",
-    ops_type: "PUB",
+    far_part: "135",
     link_to_apply: "http://transairhawaii.com/careers",
     coordinates: {
-      lat: 21.322830,
-      lng: -157.908900
-    }
-},
-{
+      lat: 21.32283,
+      lng: -157.9089,
+    },
+  },
+  {
     dsgn_code: "HALA",
     company_name: "Hawaiian Airlines",
     address: "3375 Koapaka Street, G-350",
@@ -64,14 +64,14 @@ const airOpsTestList = [
     zip: "96819",
     number_of_planes: "62",
     yib: "1929-10-06",
-    ops_type: "PUB",
+    far_part: "135",
     link_to_apply: "https://rn11.ultipro.com/HAW1000/JobBoard",
     coordinates: {
-      lat: 21.336300,
-      lng: -157.917420
-    }
-},
-{
+      lat: 21.3363,
+      lng: -157.91742,
+    },
+  },
+  {
     dsgn_code: "MK9A",
     company_name: "Mokulele Airlines",
     address: "355 Hukilike Street, Suite 103",
@@ -80,180 +80,180 @@ const airOpsTestList = [
     zip: "96732",
     number_of_planes: "12",
     yib: "1994-06-15",
-    ops_type: "PUB",
+    far_part: "135",
     link_to_apply:
-    "https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=fbb94cb3-cbb5-4d4b-8225-770829b92d51&ccId=19000101_000001&lang=en_US",
+      "https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=fbb94cb3-cbb5-4d4b-8225-770829b92d51&ccId=19000101_000001&lang=en_US",
     coordinates: {
-      lat: 20.884690,
-      lng: -156.459110
-    }
-},
-{
-  dsgn_code: "TSAA",
-  company_name: "Aloha Air Cargo",
-  address: "50 Elliott Street",
-  city: "Honolulu",
-  state: "HI",
-  zip: "96819",
-  number_of_planes: "3",
-  yib: "2008-05-15",
-  ops_type: "PUB",
-  link_to_apply: "https://www.alohaaircargo.com/careers/",
-  coordinates: {
-    lat: 21.338280,
-    lng: -157.926730
-  }
-},
-{
-  dsgn_code: "I5PA",
-  company_name: "Asia Pacific Airlines",
-  address: "155 Kapalulu Place, Suite 203",
-  city: "Honolulu",
-  state: "HI",
-  zip: "96819",
-  number_of_planes: "3",
-  yib: "1999-06-03",
-  ops_type: "PUB",
-  link_to_apply: "https://www.asiapacificairlines.com/careers/",
-  coordinates: {
-    lat: 21.320590,
-    lng: -157.910800
-  }
-},
-{
-  dsgn_code: "JRAA",
-  company_name: "Trans Executive Airlines",
-  address: "100 Iolana Place",
-  city: "Honolulu",
-  state: "HI",
-  zip: "96819",
-  number_of_planes: "10",
-  yib: "1982-06-15",
-  ops_type: "PUB",
-  link_to_apply: "http://transairhawaii.com/careers",
-  coordinates: {
-    lat: 21.322830,
-    lng: -157.908900
-  }
-},
-{
-  dsgn_code: "HALA",
-  company_name: "Hawaiian Airlines",
-  address: "3375 Koapaka Street, G-350",
-  city: "Honolulu",
-  state: "HI",
-  zip: "96819",
-  number_of_planes: "62",
-  yib: "1929-10-06",
-  ops_type: "PUB",
-  link_to_apply: "https://rn11.ultipro.com/HAW1000/JobBoard",
-  coordinates: {
-    lat: 21.336300,
-    lng: -157.917420
-  }
-},
-{
-  dsgn_code: "MK9A",
-  company_name: "Mokulele Airlines",
-  address: "355 Hukilike Street, Suite 103",
-  city: "Kahului",
-  state: "HI",
-  zip: "96732",
-  number_of_planes: "12",
-  yib: "1994-06-15",
-  ops_type: "PUB",
-  link_to_apply:
-  "https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=fbb94cb3-cbb5-4d4b-8225-770829b92d51&ccId=19000101_000001&lang=en_US",
-  coordinates: {
-    lat: 20.884690,
-    lng: -156.459110
-  }
-},
-{
-  dsgn_code: "TSAA",
-  company_name: "Aloha Air Cargo",
-  address: "50 Elliott Street",
-  city: "Honolulu",
-  state: "HI",
-  zip: "96819",
-  number_of_planes: "3",
-  yib: "2008-05-15",
-  ops_type: "PUB",
-  link_to_apply: "https://www.alohaaircargo.com/careers/",
-  coordinates: {
-    lat: 21.338280,
-    lng: -157.926730
-  }
-},
-{
-  dsgn_code: "I5PA",
-  company_name: "Asia Pacific Airlines",
-  address: "155 Kapalulu Place, Suite 203",
-  city: "Honolulu",
-  state: "HI",
-  zip: "96819",
-  number_of_planes: "3",
-  yib: "1999-06-03",
-  ops_type: "PUB",
-  link_to_apply: "https://www.asiapacificairlines.com/careers/",
-  coordinates: {
-    lat: 21.320590,
-    lng: -157.910800
-  }
-},
-{
-  dsgn_code: "JRAA",
-  company_name: "Trans Executive Airlines",
-  address: "100 Iolana Place",
-  city: "Honolulu",
-  state: "HI",
-  zip: "96819",
-  number_of_planes: "10",
-  yib: "1982-06-15",
-  ops_type: "PUB",
-  link_to_apply: "http://transairhawaii.com/careers",
-  coordinates: {
-    lat: 21.322830,
-    lng: -157.908900
-  }
-},
-{
-  dsgn_code: "HALA",
-  company_name: "Hawaiian Airlines",
-  address: "3375 Koapaka Street, G-350",
-  city: "Honolulu",
-  state: "HI",
-  zip: "96819",
-  number_of_planes: "62",
-  yib: "1929-10-06",
-  ops_type: "PUB",
-  link_to_apply: "https://rn11.ultipro.com/HAW1000/JobBoard",
-  coordinates: {
-    lat: 21.336300,
-    lng: -157.917420
-  }
-},
-{
-  dsgn_code: "MK9A",
-  company_name: "Mokulele Airlines",
-  address: "355 Hukilike Street, Suite 103",
-  city: "Kahului",
-  state: "HI",
-  zip: "96732",
-  number_of_planes: "12",
-  yib: "1994-06-15",
-  ops_type: "PUB",
-  link_to_apply:
-  "https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=fbb94cb3-cbb5-4d4b-8225-770829b92d51&ccId=19000101_000001&lang=en_US",
-  coordinates: {
-    lat: 20.884690,
-    lng: -156.459110
-  }
-},
+      lat: 20.88469,
+      lng: -156.45911,
+    },
+  },
+  {
+    dsgn_code: "TSAA",
+    company_name: "Aloha Air Cargo",
+    address: "50 Elliott Street",
+    city: "Honolulu",
+    state: "HI",
+    zip: "96819",
+    number_of_planes: "3",
+    yib: "2008-05-15",
+    far_part: "121",
+    link_to_apply: "https://www.alohaaircargo.com/careers/",
+    coordinates: {
+      lat: 21.33828,
+      lng: -157.92673,
+    },
+  },
+  {
+    dsgn_code: "I5PA",
+    company_name: "Asia Pacific Airlines",
+    address: "155 Kapalulu Place, Suite 203",
+    city: "Honolulu",
+    state: "HI",
+    zip: "96819",
+    number_of_planes: "3",
+    yib: "1999-06-03",
+    far_part: "121",
+    link_to_apply: "https://www.asiapacificairlines.com/careers/",
+    coordinates: {
+      lat: 21.32059,
+      lng: -157.9108,
+    },
+  },
+  {
+    dsgn_code: "JRAA",
+    company_name: "Trans Executive Airlines",
+    address: "100 Iolana Place",
+    city: "Honolulu",
+    state: "HI",
+    zip: "96819",
+    number_of_planes: "10",
+    yib: "1982-06-15",
+    far_part: "121",
+    link_to_apply: "http://transairhawaii.com/careers",
+    coordinates: {
+      lat: 21.32283,
+      lng: -157.9089,
+    },
+  },
+  {
+    dsgn_code: "HALA",
+    company_name: "Hawaiian Airlines",
+    address: "3375 Koapaka Street, G-350",
+    city: "Honolulu",
+    state: "HI",
+    zip: "96819",
+    number_of_planes: "62",
+    yib: "1929-10-06",
+    far_part: "121",
+    link_to_apply: "https://rn11.ultipro.com/HAW1000/JobBoard",
+    coordinates: {
+      lat: 21.3363,
+      lng: -157.91742,
+    },
+  },
+  {
+    dsgn_code: "MK9A",
+    company_name: "Mokulele Airlines",
+    address: "355 Hukilike Street, Suite 103",
+    city: "Kahului",
+    state: "HI",
+    zip: "96732",
+    number_of_planes: "12",
+    yib: "1994-06-15",
+    far_part: "121",
+    link_to_apply:
+      "https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=fbb94cb3-cbb5-4d4b-8225-770829b92d51&ccId=19000101_000001&lang=en_US",
+    coordinates: {
+      lat: 20.88469,
+      lng: -156.45911,
+    },
+  },
+  {
+    dsgn_code: "TSAA",
+    company_name: "Aloha Air Cargo",
+    address: "50 Elliott Street",
+    city: "Honolulu",
+    state: "HI",
+    zip: "96819",
+    number_of_planes: "3",
+    yib: "2008-05-15",
+    far_part: "125",
+    link_to_apply: "https://www.alohaaircargo.com/careers/",
+    coordinates: {
+      lat: 21.33828,
+      lng: -157.92673,
+    },
+  },
+  {
+    dsgn_code: "I5PA",
+    company_name: "Asia Pacific Airlines",
+    address: "155 Kapalulu Place, Suite 203",
+    city: "Honolulu",
+    state: "HI",
+    zip: "96819",
+    number_of_planes: "3",
+    yib: "1999-06-03",
+    far_part: "125",
+    link_to_apply: "https://www.asiapacificairlines.com/careers/",
+    coordinates: {
+      lat: 21.32059,
+      lng: -157.9108,
+    },
+  },
+  {
+    dsgn_code: "JRAA",
+    company_name: "Trans Executive Airlines",
+    address: "100 Iolana Place",
+    city: "Honolulu",
+    state: "HI",
+    zip: "96819",
+    number_of_planes: "10",
+    yib: "1982-06-15",
+    far_part: "125",
+    link_to_apply: "http://transairhawaii.com/careers",
+    coordinates: {
+      lat: 21.32283,
+      lng: -157.9089,
+    },
+  },
+  {
+    dsgn_code: "HALA",
+    company_name: "Hawaiian Airlines",
+    address: "3375 Koapaka Street, G-350",
+    city: "Honolulu",
+    state: "HI",
+    zip: "96819",
+    number_of_planes: "62",
+    yib: "1929-10-06",
+    far_part: "125",
+    link_to_apply: "https://rn11.ultipro.com/HAW1000/JobBoard",
+    coordinates: {
+      lat: 21.3363,
+      lng: -157.91742,
+    },
+  },
+  {
+    dsgn_code: "MK9A",
+    company_name: "Mokulele Airlines",
+    address: "355 Hukilike Street, Suite 103",
+    city: "Kahului",
+    state: "HI",
+    zip: "96732",
+    number_of_planes: "12",
+    yib: "1994-06-15",
+    far_part: "125",
+    link_to_apply:
+      "https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=fbb94cb3-cbb5-4d4b-8225-770829b92d51&ccId=19000101_000001&lang=en_US",
+    coordinates: {
+      lat: 20.88469,
+      lng: -156.45911,
+    },
+  },
 ];
 
 // Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
+app.get("/api/passwords", (req, res) => {
   const count = 5;
 
   // Return them as json
@@ -264,8 +264,8 @@ app.get('/api/passwords', (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
 });
 
 const port = process.env.PORT || 5000;
